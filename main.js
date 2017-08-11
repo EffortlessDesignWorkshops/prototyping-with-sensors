8dfa51d110cb758aba4d2eae8c0a8d16c19d728a
Garrett Scott <garrett.a.scott@gmail.com>
2017-08-11 14:15:02 -0700

    Added some nice closing/opening of the window, this was holding on to the serial port after things closed.

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ board.initialize();
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
       height: 600,
-      width: 800
+      width: 800,
+      show: false
   });
 
   mainWindow.loadURL('file://' + __dirname + '/public/index.html');
   var test = 0;
+
   board.Board.on('board-opened', () => {
     console.log('Yupp...');
   }).on('new-data', (d) => {
@@ -22,6 +24,12 @@ app.on('ready', function() {
       mainWindow.webContents.send('new-data', matches[1]);
 
     }
+  })
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  }).on('close', () => {
+    mainWindow = null;
+    board.closeOut();
   })
   
 });
