9e07803e53dcdbabc57b158b8e6f0c4eef5ebe54
Garrett Scott <garrett.a.scott@gmail.com>
2017-09-14 09:41:18 -0700

    Cleaned up code a lot and allow for swapping a board out nicely

--- main.js
+++ main.js
@@ -1,9 +1,57 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain} = require('electron');
 const board = require("./board");
 
 let mainWindow;
 
 board.initialize();
+
+function setSerialBoardStatus(status) { if(mainWindow) { mainWindow.webContents.send('serial-board-status', status); mainWindow.__newSerialConnection = false;}}
+function serialBoardOpenHandler() {
+  console.log('Opening Up Board...');
+  mainWindow.__newSerialConnection = true;
+  console.log('visible?',mainWindow.isVisible());
+  if(mainWindow.isVisible()){
+    setSerialBoardStatus(true)
+  }
+  board.Board.on('new-data', serialBoardDataHandler);
+}
+function serialBoardDataHandler(d) {
+  var matches = d.match(/EKG:([\-\d]+)/);
+  if(matches){
+    mainWindow.webContents.send('new-data', matches[1]);
+  }
+}
+function serialBoardCloseHandler() {
+  if(mainWindow){
+    console.log('Bye bye board! We will see you when you get back!')
+    setSerialBoardStatus(false);
+    board.Board.removeAllListeners('new-data')
+    board.pollForBoard();
+  }
+}
+function setupSerialBoardEvents(){
+  board.Board
+    .on('board-opened', serialBoardOpenHandler)
+    .on('board-closed', serialBoardCloseHandler)
+}
+
+function exitApp(){
+  mainWindow = null;
+  ipcMain.removeAllListeners('board-action');
+  board.closeOut();
+  app.exit()
+}
+
+var flow = true
+function toggleBoardData(){
+  flow = !flow;
+  board.setDataFlowState(flow);
+}
+
+const btn_action_map = {
+  'exit': exitApp,
+  'start-stop': toggleBoardData
+}
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
@@ -11,26 +59,23 @@ app.on('ready', function() {
       width: 800,
       show: false
   });
-
   mainWindow.loadURL('file://' + __dirname + '/public/index.html');
-  var test = 0;
-
-  board.Board.on('board-opened', () => {
-    console.log('Yupp...');
-  }).on('new-data', (d) => {
-    // console.log(d);
-    var matches = d.match(/EKG:([\-\d]+)/);
-    if(matches){
-      mainWindow.webContents.send('new-data', matches[1]);
-
-    }
+  setupSerialBoardEvents();
+  ipcMain.on('board-action', (event, action) => {
+    console.log('From main', action);
+    (btn_action_map[action] || (() => {}))();
   })
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
-  }).on('close', () => {
-    mainWindow = null;
-    board.closeOut();
-    app.exit()
-  })
+
+  mainWindow
+    .once('ready-to-show', () => {
+      mainWindow.show()
+    })
+    .on('show', () => {
+      if(mainWindow.__newSerialConnection){
+        console.log('delayed board start');
+        setSerialBoardStatus(true);
+      }
+    })
+    .on('close', exitApp)
   
 });
